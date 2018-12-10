7c817831304d7cf1c455983ffae5915a6c64d6ce
wangyalijas <1114920411@qq.com>
2018-12-10 12:50:47 +0800

    flexible 依赖

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import Mint from 'mint-ui';
+import { Select } from 'element-ui';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.use(Mint);
-
+Vue.use(Select);
 Vue.config.productionTip = false;
 
 new Vue({
