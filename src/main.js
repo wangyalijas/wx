460c3f767bcbfb968b5888d14575fcfa9e2f3ecc
wangyalijas <1114920411@qq.com>
2018-12-10 13:06:12 +0800

    delete babel

--- src/main.js
+++ src/main.js
@@ -9,6 +9,13 @@ Vue.use(Mint);
 Vue.use(Select);
 Vue.config.productionTip = false;
 
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  }
+  next();
+});
+
 new Vue({
   router,
   store,
