c4f5fffb382801f00ee1603f0284126c13523c82
wangyalijas <1114920411@qq.com>
2018-12-11 16:50:35 +0800

    提交简历

---
+++ src/components/job/Education.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="education" v-if="insideIsShowEducation">
+    <div class="education__title">教育经历</div>
+    <div class="education__info">
+      <mt-field label="开始时间" placeholder="请选择"></mt-field>
+      <mt-field label="结束时间" placeholder="请选择"></mt-field>
+      <mt-field label="学校" placeholder="请填写毕业院校" type="tel"></mt-field>
+      <mt-field label="专业" placeholder="请填写专业" type="email"></mt-field>
+      <mt-field label="学历" placeholder="请选择学历" type="date"></mt-field>
+    </div>
+    <div class="footer">
+      <span class="footer-value" @click="writed">提交</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'education',
+  props: {
+    isShowEducation: Boolean,
+  },
+  data() {
+    return {
+      insideIsShowEducation: false,
+    };
+  },
+  watch: {
+    isShowEducation() {
+      this.initialInsideValue();
+    },
+  },
+  methods: {
+    initialInsideValue(value = this.isShowEducation) {
+      this.insideIsShowEducation = value;
+    },
+    writed() {
+      this.$emit('writedEducation', true);
+    },
+  },
+  created() {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .education {
+    position: absolute;
+    top: 0;
+    background: #ffffff;
+    width: 100%;
+    height: 100vh;
+    @include e(title) {
+      font-size: 0.43rem;
+      color: #333333;
+      font-weight: bold;
+      padding: 0.53rem 0.53rem;
+    }
+    @include e(info){
+      position: relative;
+      padding: 0 0.53rem;
+      .mint-cell{
+        .mint-cell-wrapper{
+          border-bottom: 0.03rem solid #F5F5F5;
+          .mint-cell-title{
+            text-align: left;
+          }
+          .mint-cell-value .mint-field-core{
+            text-align: right;
+            margin-right: 0.27rem;
+          }
+        }
+        border-bottom: 1px solid #F5F5F5;
+      }
+    }
+    .footer{
+      position: fixed;
+      bottom: 0;
+      width: 10.00rem;
+      height: 1.33rem;
+      background: #6EA4FF;
+      box-shadow: 0 -0.05rem 0.16rem 0 rgba(212,212,212,0.30);
+      text-align: center;
+      .footer-value{
+        font-size: 0.43rem;
+        color: #FFFFFF;
+        line-height: 1.33rem;
+      }
+    }
+  }
+</style>
--- src/views/job/JobResume.vue
+++ src/views/job/JobResume.vue
@@ -1,38 +1,75 @@
 <template>
-  <div class="job-resume">
-    <div class="job-resume__title">个人信息</div>
-    <div class="job-resume__info">
-      <mt-field label="姓名" placeholder="请填写姓名"></mt-field>
-      <mt-field label="性别" placeholder="请填写身份证"></mt-field>
-      <mt-field label="电话号码" placeholder="请填写电话号码" type="tel"></mt-field>
-      <mt-field label="电子邮件" placeholder="请填写电子邮件" type="email"></mt-field>
+  <div>
+    <div class="job-resume" :class="{'opcity': isShowEducation || isShowJobExperience}">
+      <div class="job-resume__title">个人信息</div>
+      <div class="job-resume__info">
+        <mt-field label="姓名" placeholder="请填写姓名"></mt-field>
+        <mt-field label="性别" placeholder="请选择"></mt-field>
+        <mt-field label="电话号码" placeholder="请填写电话号码" type="tel"></mt-field>
+        <mt-field label="电子邮件" placeholder="请填写电子邮件" type="email"></mt-field>
+        <mt-field label="参加工作年月" placeholder="请选择" type="date" v-model="form.date"></mt-field>
+      </div>
+      <div class="job-resume__education">
+        <div class="job-resume__education--title">教育经历</div>
+        <div class="job-resume__education--button" @click="isShowEducation = true">+ 添加教育经历</div>
+      </div>
+      <div class="job-resume__education">
+        <div class="job-resume__education--title">工作经历</div>
+        <div class="job-resume__education--button"
+        @click="isShowJobExperience = true">+ 添加工作经历</div>
+      </div>
+      <div class="job-resume__img">
+        <div class="job-resume__img--title">附件</div>
+        <div class="job-resume__img--button">+</div>
+        <div class="job-resume__img--tip">提示：图片必须小于2M</div>
+      </div>
+      <div class="footer">
+        <span class="footer-value">提交</span>
+      </div>
     </div>
-    <div class="job-resume__education">
-      <div class="job-resume__education--title">教育经历</div>
-      <div class="job-resume__education--button">+ 添加教育经历</div>
-    </div>
-    <div class="job-resume__education">
-      <div class="job-resume__education--title">工作经历</div>
-      <div class="job-resume__education--button">+ 添加工作经历</div>
-    </div>
-    <div class="job-resume__img">
-      <div class="job-resume__img--title">附件</div>
-      <div class="job-resume__img--button">+</div>
-      <div class="job-resume__img--tip">提示：图片必须小于2M</div>
-    </div>
-    <div class="footer">
-      <span class="footer-value">提交</span>
-    </div>
+    <education
+     :isShowEducation="isShowEducation"
+     @writedEducation="writedEducation"></education>
+    <job-experience
+    :isShowJobExperience="isShowJobExperience"
+    @writedJobExperience="writedJobExperience"></job-experience>
   </div>
 </template>
 
 <script>
+import Education from '../../components/job/Education';
+import JobExperience from '../../components/job/JobExperience';
+
 export default {
   name: 'JobResume',
+  data() {
+    return {
+      form: {
+        date: '',
+      },
+      isShowEducation: false,
+      isShowJobExperience: false,
+    };
+  },
+  methods: {
+    writedEducation() {
+      this.isShowEducation = false;
+    },
+    writedJobExperience() {
+      this.isShowJobExperience = false;
+    },
+  },
+  components: {
+    Education,
+    JobExperience,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.opcity {
+  opacity: 0;
+}
 .job-resume {
   position: relative;
   background: #ffffff;
