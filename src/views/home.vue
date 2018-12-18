65e423d5d23d05a0edc8102c6d73fa2e505aa698
jjxiang <2331767972@qq.com>
2018-12-18 16:22:21 +0800

    fix: axios

---
+++ src/views/home.vue
@@ -0,0 +1,21 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {};
+  },
+  computed: {
+  },
+  methods: {
+  },
+  components: {
+  },
+  created() {
+    this.$nextTick(() => {
+    });
+  },
+};
+</script>
